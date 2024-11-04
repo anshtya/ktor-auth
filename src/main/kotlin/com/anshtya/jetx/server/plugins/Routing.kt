e8d86360a4b34d3f7a7e6a5edfee628fc12bf5ec
Ansh Tyagi <tyagiansh486@gmail.com>
2024-11-04 09:48:15 +0530

    Protect /user/{username} with authenticate

--- src/main/kotlin/com/anshtya/jetx/server/plugins/Routing.kt
+++ src/main/kotlin/com/anshtya/jetx/server/plugins/Routing.kt
@@ -4,7 +4,9 @@ import com.anshtya.jetx.server.controllers.AuthController
 import com.anshtya.jetx.server.repository.UserRepository
 import com.anshtya.jetx.server.routes.authRoute
 import com.anshtya.jetx.server.services.JwtService
+import com.anshtya.jetx.server.util.AUTH_JWT
 import io.ktor.server.application.*
+import io.ktor.server.auth.authenticate
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.*
@@ -20,11 +22,13 @@ fun Application.configureRouting(
             call.respondText("Hello World!")
         }
 
-        get("/user/{username}") {
-            val username = call.parameters["username"]
-            val user = userRepository.getUser(username!!)
+        authenticate(AUTH_JWT) {
+            get("/user/{username}") {
+                val username = call.parameters["username"]
+                val user = userRepository.getUser(username!!)
 
-            call.respond(mapOf("user" to user))
+                call.respond(mapOf("user" to user?.username))
+            }
         }
     }
 }
